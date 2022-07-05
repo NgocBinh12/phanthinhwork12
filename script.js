1979e1ee10ce07f4e2d069c90ad2f1715c1d4903
Ngọc Bình <ngocbinh12403@email.com>
2022-07-05 10:51:12 +0700

    binh

--- script.js
+++
@@ -1,7 +0,0 @@
-const hambuger = document.querySelector(".menu-icons");
-const navlist = document.querySelector(".nav-list");
-hambuger.addEventListener("click", () => {
-
-    hambuger.classList.toggle("acctive");
-    navlist.classList.toggle("active");
-});
